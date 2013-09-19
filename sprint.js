28395a71435ab4e3266bb914635d969dbc70b0dc
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2013-09-19 13:54:08 +0900

    take some func from teppeis

--- sprint.js
+++ sprint.js
@@ -14,6 +14,29 @@ function getCookies(){
     return Game.cookies
 };
 
+// derived from https://gist.github.com/teppeis/6576829
+var defaultCPS = [0.5, 0.5, 2, 10, 40, 100, 400, 6666, 98765, 999999];
+
+function toNum(str) {
+    return Number(str.replace(/,/g, ''));
+}
+
+function getCPS(num) {
+    var el = document.getElementById('rowInfoContent' + num);
+    if (!el) return defaultCPS[num];
+    var match = /• ([\d,]+)[\s\S]*producing ([\d,.]+)/.exec(el.innerText);
+    if (match[1] === '0') {
+        return defaultCPS[num];
+    }
+    return toNum(match[2]) / toNum(match[1]);
+}
+
+function getCost(num) {
+  var a = document.querySelector('#product' + num + ' .price').innerText;
+  return toNum(a);
+}
+// end: derived from https://gist.github.com/teppeis/6576829
+
 function startSprint($, goal){
     // Game.HardReset
     for (var i in Game.AchievementsById){
